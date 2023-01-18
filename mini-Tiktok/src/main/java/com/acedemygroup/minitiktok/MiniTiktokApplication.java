2af3ed87490328f45614c236f6db1577e374b2b7
joeCarf <qiao_ao@foxmail.com>
2023-01-18 22:03:11 +0800

    init

---
+++ mini-Tiktok/src/main/java/com/acedemygroup/minitiktok/MiniTiktokApplication.java
@@ -0,0 +1,13 @@
+package com.acedemygroup.minitiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTiktokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniTiktokApplication.class, args);
+    }
+
+}
---
+++ mini-Tiktok/src/test/java/com/acedemygroup/minitiktok/MiniTiktokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acedemygroup.minitiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTiktokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
