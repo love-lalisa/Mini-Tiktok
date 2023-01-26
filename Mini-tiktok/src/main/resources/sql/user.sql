23b3780d1a67a948e1d8be8be767f28a77c87e5b
love-lalisa <2498138023@qq.com>
2023-01-26 16:37:32 +0800

    add sql file

--- Mini-tiktok/src/main/resources/sql/user.sql
+++ Mini-tiktok/src/main/resources/sql/user.sql
@@ -1,15 +1,16 @@
 
-
-
-
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for tb_user
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_user`;
 CREATE TABLE `tb_user`  (
-  `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键，用户id',
   `user_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '昵称',
   `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '密码',
-   PRIMARY KEY (`user_id`) ,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1010 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
--- Mini-tiktok/src/main/resources/sql/userInfo.sql
+++ Mini-tiktok/src/main/resources/sql/userInfo.sql
@@ -1,13 +1,14 @@
 
-
 -- ----------------------------
 -- Table structure for tb_user_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_user_info`;
-CREATE TABLE `tb_user_info`  (
-  `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户id',
+CREATE TABLE `tb_user_info`(
+  `user_id` bigint(20) UNSIGNED NOT NULL COMMENT '用户id',
   `user_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '昵称',
   `follow_count` int(8) UNSIGNED NULL DEFAULT 0 COMMENT '关注总数',
   `follower_count` int(8) UNSIGNED NULL DEFAULT 0 COMMENT '粉丝总数',
-  PRIMARY KEY (`user_id`) ,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1010 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;
